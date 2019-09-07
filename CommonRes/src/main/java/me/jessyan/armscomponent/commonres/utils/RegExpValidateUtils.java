d5828828d3a01f6ddc4cab5689ce7c4822fe0ae0
lanjian <819715035@qq.com>
2019-09-07 11:03:43 +0800

    增加添加银行卡页面

---
+++ CommonRes/src/main/java/me/jessyan/armscomponent/commonres/utils/RegExpValidateUtils.java
@@ -0,0 +1,202 @@
+package me.jessyan.armscomponent.commonres.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author lanjian
+ * creat at 2019/9/7
+ * description 正则表达工具类
+ */
+public final class RegExpValidateUtils {
+    /**
+     * 验证输入手机号码
+     */
+    public static boolean IsHandset(String str) {
+        String regex = "^[1]+[3,5]+\\d{9}$";
+        return match(regex, str);
+    }
+
+    /**
+     * 验证输入密码条件(字符与数据同时出现)
+     */
+    public static boolean IsPassword(String str) {
+        String regex = "[A-Za-z]+[0-9]";
+        return match(regex, str);
+    }
+
+    /**
+     * @param regex
+     * 正则表达式字符串
+     * @param str
+     * 要匹配的字符串
+     * @return 如果str 符合 regex的正则表达式格式,返回true, 否则返回 false;
+     */
+    private static boolean match(String regex, String str) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+
+    /**
+     11      * 正则表达式:验证用户名(不包含中文和特殊字符)如果用户名使用手机号码或邮箱 则结合手机号验证和邮箱验证
+     12      */
+    public static final String REGEX_USERNAME = "^[a-zA-Z]\\w{5,17}$";
+    /**
+     * 正则表达式:验证密码(不包含特殊字符)
+     */
+    public static final String REGEX_PASSWORD = "^[a-zA-Z0-9]{1,12}$";
+
+    /**
+     * 正则表达式:验证手机号
+     */
+    public static final String REGEX_MOBILE = "^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$";
+
+    /**
+     * 正则表达式:验证邮箱
+     */
+    public static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+
+    /**
+     * 正则表达式:验证汉字(1-9个汉字)  {1,9} 自定义区间
+     */
+    public static final String REGEX_CHINESE = "^[\u4e00-\u9fa5]{1,9}$";
+
+    /**
+     * 正则表达式:验证身份证
+     */
+    public static final String REGEX_ID_CARD = "(\\d{14}[0-9a-zA-Z])|(\\d{17}[0-9a-zA-Z])";
+
+    /**
+     * 正则表达式:验证URL
+     */
+    public static final String REGEX_URL = "http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?";
+
+    /**
+     * 正则表达式:验证IP地址
+     */
+    public static final String REGEX_IP_ADDR = "(2[5][0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})\\.(25[0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})\\.(25[0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})\\.(25[0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})";
+
+    /**
+     * 校验用户名
+     *
+     * @param username
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isUserName(String username) {
+        return Pattern.matches(REGEX_USERNAME, username);
+    }
+
+    /**
+     * 校验密码
+     *
+     * @param password
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isPassword(String password) {
+        return Pattern.matches(REGEX_PASSWORD, password);
+    }
+
+    /**
+     * 校验手机号
+     *
+     * @param mobile
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isMobile(String mobile) {
+        return Pattern.matches(REGEX_MOBILE, mobile);
+    }
+
+    /**
+     * 校验邮箱
+     *
+     * @param email
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isEmail(String email) {
+        return Pattern.matches(REGEX_EMAIL, email);
+    }
+
+    /**
+     * 校验汉字
+     *
+     * @param chinese
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isChinese(String chinese) {
+        return Pattern.matches(REGEX_CHINESE, chinese);
+    }
+
+    /**
+     * 校验身份证
+     *
+     * @param idCard
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isIDCard(String idCard) {
+        return Pattern.matches(REGEX_ID_CARD, idCard);
+    }
+
+    /**
+     * 校验URL
+     *
+     * @param url
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isUrl(String url) {
+        return Pattern.matches(REGEX_URL, url);
+    }
+    /**
+     * 校验IP地址
+     *
+     * @param ipAddress
+     * @return
+     */
+    public static boolean isIPAddress(String ipAddress) {
+        return Pattern.matches(REGEX_IP_ADDR, ipAddress);
+    }
+
+     /*
+    校验过程：
+    1、从卡号最后一位数字开始，逆向将奇数位(1、3、5等等)相加。
+    2、从卡号最后一位数字开始，逆向将偶数位数字，先乘以2（如果乘积为两位数，将个位十位数字相加，即将其减去9），再求和。
+    3、将奇数位总和加上偶数位总和，结果应该可以被10整除。
+    */
+    /**
+     * 校验银行卡卡号
+     */
+    public static boolean checkBankCard(String bankCard) {
+        if(bankCard.length() < 15 || bankCard.length() > 19) {
+            return false;
+        }
+        char bit = getBankCardCheckCode(bankCard.substring(0, bankCard.length() - 1));
+        if(bit == 'N'){
+            return false;
+        }
+        return bankCard.charAt(bankCard.length() - 1) == bit;
+    }
+
+    /**
+     * 从不含校验位的银行卡卡号采用 Luhm 校验算法获得校验位
+     * @param nonCheckCodeBankCard
+     * @return
+     */
+    public static char getBankCardCheckCode(String nonCheckCodeBankCard){
+        if(nonCheckCodeBankCard == null || nonCheckCodeBankCard.trim().length() == 0
+                || !nonCheckCodeBankCard.matches("\\d+")) {
+            //如果传的不是数据返回N
+            return 'N';
+        }
+        char[] chs = nonCheckCodeBankCard.trim().toCharArray();
+        int luhmSum = 0;
+        for(int i = chs.length - 1, j = 0; i >= 0; i--, j++) {
+            int k = chs[i] - '0';
+            if(j % 2 == 0) {
+                k *= 2;
+                k = k / 10 + k % 10;
+            }
+            luhmSum += k;
+        }
+        return (luhmSum % 10 == 0) ? '0' : (char)((10 - luhmSum % 10) + '0');
+    }
+}
--- module_main/src/main/java/com/ooo/main/mvp/ui/activity/AddBlankCardActivity.java
+++ module_main/src/main/java/com/ooo/main/mvp/ui/activity/AddBlankCardActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +20,8 @@ import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
 import com.bigkoo.pickerview.listener.CustomListener;
 import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
 import com.bigkoo.pickerview.view.OptionsPickerView;
+import com.blankj.utilcode.util.RegexUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
 import com.ooo.main.R;
@@ -30,6 +35,9 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import me.jessyan.armscomponent.commonres.dialog.BaseCustomDialog;
+import me.jessyan.armscomponent.commonres.dialog.BaseDialog;
+import me.jessyan.armscomponent.commonres.utils.RegExpValidateUtils;
 import me.jessyan.armscomponent.commonsdk.base.BaseSupportActivity;
 import me.jessyan.armscomponent.commonsdk.utils.StatusBarUtils;
 
@@ -72,8 +80,36 @@ public class AddBlankCardActivity extends BaseSupportActivity <AddBlankCardPrese
     Button btnNext;
     @BindView(R2.id.rg_cardtype)
     RadioGroup rgCardtype;
-    private OptionsPickerView  pvCustomOptions;
-    private ArrayList <String> cardItem = new ArrayList<>();
+    @BindView(R2.id.iv_delect_blankName)
+    ImageView ivDelectBlankName;
+    @BindView(R2.id.iv_delect_cardNum)
+    ImageView ivDelectCardNum;
+    @BindView(R2.id.iv_delect_alipyNum)
+    ImageView ivDelectAlipyNum;
+    private OptionsPickerView pvCustomOptions;
+    private ArrayList <String> cardItem = new ArrayList <> ();
+    private BaseDialog dialog;
+
+    enum BLANKTYPE {
+        //没选中
+        NONE,
+        //支付宝
+        ALIPY,
+        //银行
+        BLANK
+    }
+
+    enum CARDTYPE {
+        //没选中
+        NONE,
+        //借记卡
+        PAYCARD,
+        //不是借记卡
+        UNPAYCARD
+    }
+
+    private BLANKTYPE blanktype = BLANKTYPE.NONE;
+    private CARDTYPE cardtype = CARDTYPE.NONE;
 
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
@@ -95,6 +131,81 @@ public class AddBlankCardActivity extends BaseSupportActivity <AddBlankCardPrese
         StatusBarUtils.setTranslucentStatus ( this );
         StatusBarUtils.setStatusBarDarkTheme ( this, true );
         tvTitle.setText ( "添加银行卡" );
+        setListener ();
+    }
+
+    private void setListener() {
+        rgCardtype.setOnCheckedChangeListener ( new RadioGroup.OnCheckedChangeListener () {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                int i1 = radioGroup.getCheckedRadioButtonId ();
+                if (i1 == R.id.rb_yes) {
+                    cardtype = CARDTYPE.PAYCARD;
+                } else if (i1 == R.id.rb_no) {
+                    cardtype = CARDTYPE.UNPAYCARD;
+                }
+            }
+        } );
+        etLowerBank.addTextChangedListener ( new TextWatcher () {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (etLowerBank.getText ().toString ().trim ().length ()>0){
+                    ivDelectBlankName.setVisibility ( View.VISIBLE );
+                }else{
+                    ivDelectBlankName.setVisibility ( View.INVISIBLE );
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        } );
+        etCardNum.addTextChangedListener ( new TextWatcher () {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (etCardNum.getText ().toString ().trim ().length ()>0){
+                    ivDelectCardNum.setVisibility ( View.VISIBLE );
+                }else{
+                    ivDelectCardNum.setVisibility ( View.INVISIBLE );
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        } );
+        etAlipy.addTextChangedListener ( new TextWatcher () {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (etAlipy.getText ().toString ().trim ().length ()>0){
+                    ivDelectAlipyNum.setVisibility ( View.VISIBLE );
+                }else{
+                    ivDelectAlipyNum.setVisibility ( View.INVISIBLE );
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        } );
     }
 
     @Override
@@ -131,18 +242,99 @@ public class AddBlankCardActivity extends BaseSupportActivity <AddBlankCardPrese
         ButterKnife.bind ( this );
     }
 
-    @OnClick({R2.id.iv_back, R2.id.tv_chooseBlank, R2.id.btn_next})
+    @OnClick({R2.id.iv_back, R2.id.tv_chooseBlank, R2.id.btn_next,R2.id.iv_delect_alipyNum,
+            R2.id.iv_delect_blankName,R2.id.iv_delect_cardNum})
     public void onViewClicked(View view) {
         int i = view.getId ();
         if (i == R.id.iv_back) {
             finish ();
         } else if (i == R.id.tv_chooseBlank) {
             //选择银行
-            initCustomOptionPicker();
-            pvCustomOptions.show();
+            initCustomOptionPicker ();
+            pvCustomOptions.show ();
         } else if (i == R.id.btn_next) {
             //下一步
+            switch (blanktype) {
+                case ALIPY:
+                    String alipyNum = etAlipy.getText ().toString ().trim ();
+                    if (TextUtils.isEmpty ( alipyNum )) {
+                        ToastUtils.showShort ( "请输入支付宝账号" );
+                    } else {
+                        //检验支付宝账号
+                        if (RegexUtils.isEmail ( alipyNum ) || RegexUtils.isMobileSimple ( alipyNum )) {
+                            //通过验证
+                            showCheckDialog ();
+                        } else {
+                            //支付宝账号不对
+                            ToastUtils.showShort ( "支付宝账号格式不正确" );
+                        }
+                    }
+                    break;
+                case NONE:
+                    ToastUtils.showShort ( "请选择银行" );
+                    break;
+                case BLANK:
+                    String underBlankName = etLowerBank.getText ().toString ().trim ();
+                    String cardNum = etCardNum.getText ().toString ().trim ();
+                    if (TextUtils.isEmpty ( underBlankName )) {
+                        ToastUtils.showShort ( "请输入支行名称" );
+                        return;
+                    }
+                    if (TextUtils.isEmpty ( cardNum )) {
+                        ToastUtils.showShort ( "请输入卡号" );
+                        return;
+                    } else {
+                        if (!RegExpValidateUtils.checkBankCard ( cardNum )) {
+                            ToastUtils.showShort ( "卡号格式不正确" );
+                            return;
+                        }
+                    }
+                    switch (cardtype) {
+                        case NONE:
+                            ToastUtils.showShort ( "请选择是否为借记卡" );
+                            break;
+                        case PAYCARD:
+                            break;
+                        case UNPAYCARD:
+                            break;
+                        default:
+                            break;
+                    }
+                    showCheckDialog ();
+                    break;
+                default:
+                    break;
+            }
+        }else if(i == R.id.iv_delect_alipyNum){
+            etAlipy.setText ( "" );
+        }else if (i==R.id.iv_delect_blankName){
+            etLowerBank.setText ( "" );
+        }else if (i==R.id.iv_delect_cardNum){
+            etCardNum.setText ( "" );
         }
+    }
+
+    private void showCheckDialog() {
+        dialog = new BaseCustomDialog.Builder ( this, R.layout.dialog_submit_blankinfo, false, new BaseCustomDialog.Builder.OnShowDialogListener () {
+            @Override
+            public void onShowDialog(View layout) {
+                layout.findViewById ( R.id.tv_sure ).setOnClickListener ( new View.OnClickListener () {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss ();
+                        //确定
+                    }
+                } );
+                layout.findViewById ( R.id.tv_cancel ).setOnClickListener ( new View.OnClickListener () {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss ();
+                    }
+                } );
+            }
+        } )
+                .create ();
+        dialog.show ();
     }
 
     private void initCustomOptionPicker() {//条件选择器初始化，自定义布局
@@ -153,104 +345,108 @@ public class AddBlankCardActivity extends BaseSupportActivity <AddBlankCardPrese
          * 自定义布局中，id为 optionspicker 或者 timepicker 的布局以及其子控件必须要有，否则会报空指针。
          * 具体可参考demo 里面的两个自定义layout布局。
          */
-        pvCustomOptions = new OptionsPickerBuilder (this, new OnOptionsSelectListener () {
+        pvCustomOptions = new OptionsPickerBuilder ( this, new OnOptionsSelectListener () {
             @Override
             public void onOptionsSelect(int options1, int option2, int options3, View v) {
                 //返回的分别是三个级别的选中位置
-                String tx = cardItem.get(options1);
+                String tx = cardItem.get ( options1 );
                 tvChooseBlank.setText ( tx );
-                if (options1==0){
+                if (options1 == 0) {
                     //选中支付宝
                     llAlipy.setVisibility ( View.VISIBLE );
                     llBlank.setVisibility ( View.GONE );
-                }else{
+                    blanktype = BLANKTYPE.ALIPY;
+                    cardtype = CARDTYPE.PAYCARD;
+                } else {
                     //选中其他银行
                     llBlank.setVisibility ( View.VISIBLE );
                     llAlipy.setVisibility ( View.GONE );
+                    blanktype = BLANKTYPE.BLANK;
                 }
             }
-        })
-                .setLayoutRes(R.layout.item_choose_blank, new CustomListener () {
+        } )
+                .setLayoutRes ( R.layout.item_choose_blank, new CustomListener () {
                     @Override
                     public void customLayout(View v) {
-                        final TextView tvSubmit = (TextView) v.findViewById(R.id.tv_finish);
-                        TextView ivCancel = (TextView) v.findViewById(R.id.tv_cancel);
-                        tvSubmit.setOnClickListener(new View.OnClickListener() {
+                        final TextView tvSubmit = (TextView) v.findViewById ( R.id.tv_finish );
+                        TextView ivCancel = (TextView) v.findViewById ( R.id.tv_cancel );
+                        tvSubmit.setOnClickListener ( new View.OnClickListener () {
                             @Override
                             public void onClick(View v) {
-                                pvCustomOptions.returnData();
-                                pvCustomOptions.dismiss();
+                                pvCustomOptions.returnData ();
+                                pvCustomOptions.dismiss ();
                             }
-                        });
+                        } );
 
-                        ivCancel.setOnClickListener(new View.OnClickListener() {
+                        ivCancel.setOnClickListener ( new View.OnClickListener () {
                             @Override
                             public void onClick(View v) {
-                                pvCustomOptions.dismiss();
+                                pvCustomOptions.dismiss ();
                             }
-                        });
+                        } );
                     }
-                })
+                } )
                 .setLineSpacingMultiplier ( 2.5f )
-                .build();
-        getCardData();
-        pvCustomOptions.setPicker(cardItem);//添加数据
+                .build ();
+        getCardData ();
+        pvCustomOptions.setPicker ( cardItem );//添加数据
 
 
     }
 
     private void getCardData() {
-       cardItem.add ( "支付宝" );
-       cardItem.add ( "中国工商银行" );
-       cardItem.add ( "中国邮政储蓄银行" );
-       cardItem.add ( "中国农业银行" );
-       cardItem.add ( "中国建设银行" );
-       cardItem.add ( "中国银行" );
-       cardItem.add ( "中国民生银行" );
-       cardItem.add ( "招商银行" );
-       cardItem.add ( "兴业银行" );
-       cardItem.add ( "中信银行" );
-       cardItem.add ( "浦发银行" );
-       cardItem.add ( "中国光大银行" );
-       cardItem.add ( "广发银行" );
-       cardItem.add ( "中国交通银行" );
-       cardItem.add ( "平安银行" );
-       cardItem.add ( "华夏银行" );
-       cardItem.add ( "北京银行" );
-       cardItem.add ( "杭州银行" );
-       cardItem.add ( "汇丰银行" );
-       cardItem.add ( "上海银行" );
-       cardItem.add ( "天津银行" );
-       cardItem.add ( "南京银行" );
-       cardItem.add ( "东莞银行" );
-       cardItem.add ( "宁波银行" );
-       cardItem.add ( "无锡银行" );
-       cardItem.add ( "恒丰银行" );
-       cardItem.add ( "武汉银行" );
-       cardItem.add ( "长沙银行" );
-       cardItem.add ( "大连银行" );
-       cardItem.add ( "西安银行" );
-       cardItem.add ( "重庆银行" );
-       cardItem.add ( "济南银行" );
-       cardItem.add ( "成都银行" );
-       cardItem.add ( "贵阳银行" );
-       cardItem.add ( "石家庄银行" );
-       cardItem.add ( "昆明银行" );
-       cardItem.add ( "烟台银行" );
-       cardItem.add ( "哈尔滨银行" );
-       cardItem.add ( "郑州银行" );
-       cardItem.add ( "乌鲁木齐银行" );
-       cardItem.add ( "青岛银行" );
-       cardItem.add ( "温州银行" );
-       cardItem.add ( "合肥银行" );
-       cardItem.add ( "淄博银行" );
-       cardItem.add ( "苏州银行" );
-       cardItem.add ( "太原银行" );
-       cardItem.add ( "绍兴银行" );
-       cardItem.add ( "台州银行" );
-       cardItem.add ( "浙商银行" );
-       cardItem.add ( "临沂银行" );
-       cardItem.add ( "鞍山银行" );
-       cardItem.add ( "潍坊银行" );
+        cardItem.add ( "支付宝" );
+        cardItem.add ( "中国工商银行" );
+        cardItem.add ( "中国邮政储蓄银行" );
+        cardItem.add ( "中国农业银行" );
+        cardItem.add ( "中国建设银行" );
+        cardItem.add ( "中国银行" );
+        cardItem.add ( "中国民生银行" );
+        cardItem.add ( "招商银行" );
+        cardItem.add ( "兴业银行" );
+        cardItem.add ( "中信银行" );
+        cardItem.add ( "浦发银行" );
+        cardItem.add ( "中国光大银行" );
+        cardItem.add ( "广发银行" );
+        cardItem.add ( "中国交通银行" );
+        cardItem.add ( "平安银行" );
+        cardItem.add ( "华夏银行" );
+        cardItem.add ( "北京银行" );
+        cardItem.add ( "杭州银行" );
+        cardItem.add ( "汇丰银行" );
+        cardItem.add ( "上海银行" );
+        cardItem.add ( "天津银行" );
+        cardItem.add ( "南京银行" );
+        cardItem.add ( "东莞银行" );
+        cardItem.add ( "宁波银行" );
+        cardItem.add ( "无锡银行" );
+        cardItem.add ( "恒丰银行" );
+        cardItem.add ( "武汉银行" );
+        cardItem.add ( "长沙银行" );
+        cardItem.add ( "大连银行" );
+        cardItem.add ( "西安银行" );
+        cardItem.add ( "重庆银行" );
+        cardItem.add ( "济南银行" );
+        cardItem.add ( "成都银行" );
+        cardItem.add ( "贵阳银行" );
+        cardItem.add ( "石家庄银行" );
+        cardItem.add ( "昆明银行" );
+        cardItem.add ( "烟台银行" );
+        cardItem.add ( "哈尔滨银行" );
+        cardItem.add ( "郑州银行" );
+        cardItem.add ( "乌鲁木齐银行" );
+        cardItem.add ( "青岛银行" );
+        cardItem.add ( "温州银行" );
+        cardItem.add ( "合肥银行" );
+        cardItem.add ( "淄博银行" );
+        cardItem.add ( "苏州银行" );
+        cardItem.add ( "太原银行" );
+        cardItem.add ( "绍兴银行" );
+        cardItem.add ( "台州银行" );
+        cardItem.add ( "浙商银行" );
+        cardItem.add ( "临沂银行" );
+        cardItem.add ( "鞍山银行" );
+        cardItem.add ( "潍坊银行" );
     }
+
 }
