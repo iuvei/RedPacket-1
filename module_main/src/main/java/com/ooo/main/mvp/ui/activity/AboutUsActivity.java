f59f6c1e13ed46e34c3ed7179e4af5028248722c
819715035@qq.com <819715035@qq.com>
2019-11-09 14:27:04 +0800

    修改关于我们页面的协议

--- module_main/src/main/java/com/ooo/main/mvp/ui/activity/AboutUsActivity.java
+++ module_main/src/main/java/com/ooo/main/mvp/ui/activity/AboutUsActivity.java
@@ -10,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.blankj.utilcode.util.AppUtils;
+import com.haisheng.easeim.mvp.utils.CommontUtil;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
@@ -23,6 +24,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import me.jessyan.armscomponent.commonres.ui.WebviewActivity;
+import me.jessyan.armscomponent.commonres.utils.CommonMethod;
+import me.jessyan.armscomponent.commonres.utils.ConfigUtil;
 import me.jessyan.armscomponent.commonsdk.base.BaseSupportActivity;
 import me.jessyan.armscomponent.commonsdk.utils.StatusBarUtils;
 
@@ -78,7 +81,7 @@ public class AboutUsActivity extends BaseSupportActivity <AboutUsPresenter> impl
         StatusBarUtils.setTranslucentStatus ( this );
         StatusBarUtils.setStatusBarDarkTheme ( this, true );
         tvTitle.setText ( "关于我们" );
-        tvVersion.setText ( AppUtils.getAppVersionName () );
+        tvVersion.setText ( "盛煌"+AppUtils.getAppVersionName () );
     }
 
     @Override
@@ -121,11 +124,11 @@ public class AboutUsActivity extends BaseSupportActivity <AboutUsPresenter> impl
         if (i == R.id.iv_back) {
             finish ();
         } else if (i == R.id.ll_privacy) {
-           WebviewActivity.start ( this,"隐私协议","http://www.baidu.com" );
+          //WebviewActivity.start ( this,"隐私协议","http://www.baidu.com" );
         } else if (i == R.id.ll_user) {
-            WebviewActivity.start ( this,"用户协议","http://www.baidu.com" );
+            WebviewActivity.start ( this,"用户协议", ConfigUtil.LINK_REGISTER_ARGEEMENT );
         } else if (i == R.id.ll_withdraw) {
-            WebviewActivity.start ( this,"提现协议","http://www.baidu.com" );
+           // WebviewActivity.start ( this,"提现协议","http://www.baidu.com" );
         } else if (i == R.id.ll_version) {
             //WebviewActivity.start ( this,"版本更新","http://www.baidu.com" );
         }
